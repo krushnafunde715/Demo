052ce7322fb9cafb0cd006a22908a3f9a6b68553
KRUSHNA FUNDE <krushnafunde715@gmail.com>
2025-09-22 19:39:43 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -48,17 +48,17 @@ class Linkedlist
 }
 class Sample3
 {
- public static void main(String s[])
- {
-  	Linkedlist l1=new  Linkedlist();
-  	l1.insert(00);
-  	l1.insert(10);
-  	l1.insert(20);
-  	l1.insert(30);
-  	l1.insert(40);
-  	l1.insert(50);
-  	l1.insert(60);
-	
-  	l1.show();
- }
+	public static void main(String s[])
+    {
+  		Linkedlist l1=new  Linkedlist();
+  		l1.insert(00);
+  		l1.insert(10);
+  		l1.insert(20);
+  		l1.insert(30);
+  		l1.insert(40);
+  		l1.insert(50);
+  		l1.insert(60);
+		System.out.println("LinkedList elements are:");
+		l1.show();
+ 	}
 }
