4d18ebf26609728a6fd6cb9c462a91288ba41d80
KRUSHNA FUNDE <krushnafunde715@gmail.com>
2025-10-02 18:44:27 +0530

    Stack.java

--- Stack.java
+++ Stack.java
@@ -1,37 +1,38 @@
-class Stack
+class StackDemo
 {
     int maxSize;
-    int stackArray[];
+    int StackArray[];
     int top;
+    
     Stack(int size)
     {
         maxSize=size;
-        stackArray= new int[maxSize];
+        StackArray= new int[maxSize];
         top=-1;
     }
     public void push(int value)
     {
         if(top==maxSize-1)
         {
-            System.out.println("Stack overflow");
+            System.out.println("Stack is overflow");
         }
-        else
-        {
-            stackArray[++top]=value;
-            System.out.println(value+"is pushed in stack");
+        else{
+            StackArray[++top]=value;
+            System.out.println(value+"is pushed");
+            
         }
     }
     public int pop()
     {
         if(top==-1)
         {
-            System.out.println("Stack underflow");
+            System.out.println("Stack is underflow");
             return -1;
         }
         else
         {
-            System.out.println(stackArray[top]+"is popped");
-            return stackArray[top--];
+            System.out.println(StackArray[top]+"is popped");
+            return(top--);
         }
     }
     public void display()
@@ -45,73 +46,79 @@ class Stack
             System.out.println("Stack elements are:");
             for(int i=0; i<=top; i++)
             {
-                System.out.print(stackArray[i]+"\t");
+                System.out.print(StackArray[i]+"\t");
             }
+            System.out.println();
         }
-    }
-    public int top()
-    {
-        if(top==-1)
-        {
-            System.out.println("\n"+"Stack is empty");
-        }
-        else
-        {
-            System.out.println("\n"+"Top element is:" +stackArray[top]);
-        }   
-        return -1;
-        
-    }
-    boolean isEmpty()
-    {
-        return(top==-1);
-    }
-    boolean isFull()
-    {
-        return(top==maxSize-1);
-    }
-    public void minValue()
-    {
-        int min=stackArray[0];
-        for(int i=0; i<=top; i++)
-        {
-            if(stackArray[i]<=min)
-            {
-                min=stackArray[i];
-            }
-        }
-        System.out.println("Minimum value is:"+min);
     }
     public void maxValue()
     {
-        int max=stackArray[0];
-        for(int i=0; i<=top; i++)
+        if(top==-1)
         {
-            if(stackArray[i]>=max)
-            {
-                max=stackArray[i];
-            }
+            System.out.println("Stack is empty");
         }
-        System.out.println("Maximum value is:"+max);
+        else
+        {
+            int max=StackArray[top];
+            for(int i=0; i<=top; i++)
+            {
+                if(StackArray[i]>=max)
+                {
+                    max=StackArray[i];
+                }
+            }
+            System.out.println(max+"is the max value in the stack");
+        }
     }
-    
-}
-class StackDemo
-{
-    public static void main(String s[])
+    public void minValue()
     {
-        Stack sc= new Stack(5);
-        sc.push(10);
-        sc.push(20);
-        sc.push(30);
-        sc.pop();
-        sc.push(50);
-        sc.push(40);
-        sc.display();
-        sc.top();
-        System.out.println(sc.isEmpty());
-        System.out.println(sc.isFull());
-        sc.minValue();
-        sc.maxValue();
+        if(top==-1)
+        {
+            System.out.println("Stack is empty");
+        }
+        else
+        {
+            int min=StackArray[top];
+            for(int i=0; i<=top; i++)
+            {
+                if(StackArray[i]<=min)
+                {
+                    min=StackArray[i];
+                }
+            }
+            System.out.println(min+"is the minimum value in the stack");
+        }
     }
+    public boolean isFull()
+    {
+        return(top==maxSize-1);
+    }
+    public boolean isEmpty()
+    {
+        return(top==-1);
+    }
+}
+public class Stack
+{
+	public static void main(String[] args) {
+		StackDemo s1= new StackDemo(5);
+		
+		s1.push(10);
+		s1.push(20);
+		s1.push(30);
+		System.out.println(s1.isFull());
+		s1.push(40);
+		s1.push(15);
+		s1.push(60);
+		s1.display();
+		s1.maxValue();
+		s1.minValue();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		System.out.println(s1.isEmpty());
+		s1.pop();
+	}
 }
